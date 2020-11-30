bb5dbe60356a5b6a2f2d753c6ea2b34cdab46866
sagbeyeg <agbeyegbesisan@gmail.com>
2020-11-30 16:01:17 -0500

    routes for journals and tasks

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,20 @@ Rails.application.routes.draw do
     #   get '/profile', to: 'users#profile'
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :journals
+    #   post '/login', to: 'auth#create'
+    #   get '/profile', to: 'users#profile'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
+    #   post '/login', to: 'auth#create'
+    #   get '/profile', to: 'users#profile'
+    end
+  end
 end
